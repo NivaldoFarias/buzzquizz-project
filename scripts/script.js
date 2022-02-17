2170a9eaa69baab1ae6d9bc832862477104942b1
Luiz Gustavo Santos Farias <lgsfarias.dev@gmail.com>
2022-02-16 22:48:58 -0300

    Render user quizzes

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,11 @@ let myQuizzes = JSON.parse(localStorage.getItem("quizzes"));
 if (!myQuizzes) {
   myQuizzes = [];
 }
-let quizz = {
-  title: "Quem é você na fila do pão?",
+let myQuizzesId = myQuizzes.map(quizz => quizz.id);
+let apiQuizzes =[]
+
+const quizz = {
+  title: "teste 6?",
   image: "https://miro.medium.com/max/1400/1*CT3u9Zejnbzdg36CI5zxDg.jpeg",
   questions: [
     {
@@ -72,19 +75,63 @@ let quizz = {
   ],
 };
 
+function getMyQuizzesOnLocalStorage(){
+  myQuizzes = JSON.parse(localStorage.getItem("quizzes"));
+  if (!myQuizzes) {
+    myQuizzes = [];
+  }
+  return myQuizzes
+}
+
+function addQuizzOnLocalStorage(ID,key){
+  myQuizzes.push({ id: ID, key: key });
+  localStorage.setItem("quizzes", JSON.stringify(myQuizzes));
+}
+
+// function updateLocalStorage(quizzes){
+//   let myQuizzesId = myQuizzes.map(quizz => quizz.id)
+//   myQuizzes =[]
+//   const promise = axios.get(QUIZZ_API);
+//   promise.then(response => {
+//     response.data.forEach(quizz => {
+//       if ()
+//     })
+//   })
+
+// }
+
+
 function getAllQuizzes() {
   const promise = axios.get(QUIZZ_API);
   promise.then((response) => {
+    apiQuizzes = response.data
     console.log(response);
-    loadQuizzes(response.data);
+    loadQuizzes(apiQuizzes);
   });
 }
 
 function loadQuizzes(quizzes) {
-  if (myQuizzes.length >= 1) {
+  myQuizzesId = myQuizzes.map(quizz => quizz.id);
+  if (checkMyQuizzesOnAPI (myQuizzesId)){
     renderUserQuizzes();
   }
+  // if (myQuizzes.length >= 1) {
+  //   renderUserQuizzes();
+  // }
+  
   renderAllQuizzes(quizzes);
+}
+
+function checkMyQuizzesOnAPI (myQuizzesId) {
+  // retorna verdadeiro caso exista algum quizz na API salvo no LocalStorage
+  // e falso caso contrario
+  let quizzesId = apiQuizzes.map(quizz => quizz.id)
+  for (id of quizzesId){
+    if ( myQuizzesId.includes(id)){
+      return true
+    }
+  }
+  return false
 }
 
 function renderAllQuizzes(quizzes) {
@@ -92,14 +139,14 @@ function renderAllQuizzes(quizzes) {
 
   all_Quizzes.innerHTML = "<p>Todos os Quizzes</p>";
   quizzes.forEach((quizz, index) => {
-    if (index > 0 && quizz.image !== quizzes[index - 1].image) {
+    // if (index > 0 && quizz.image !== quizzes[index - 1].image) {
       all_Quizzes.innerHTML += `
       <article>
         <img src="${quizz.image}" alt="imagem do quizz" />
         <div class="gradient"></div>
         <p>${quizz.title}</p>
       </article>`;
-    }
+    // }
   });
 }
 
@@ -108,14 +155,18 @@ function renderUserQuizzes() {
   const no_Quizzes = document.querySelector(".no-quizz-available");
   no_Quizzes.classList.add("hidden");
   user_Quizzes.classList.remove("hidden");
+  user_Quizzes.innerHTML=`<p>Seus Quizzes</p>
+        <ion-icon name="add-circle" id="create-quizz-btn"></ion-icon>`
 
-  user_Quizzes.forEach((quizz) => {
-    user_Quizzes.innerHTML += `
-    <article>
-      <img src="${quizz.image}" alt="imagem do quizz" />
-      <div class="gradient"></div>
-      <p>${quizz.title}</p>
-    </article>`;
+  apiQuizzes.forEach((quizz) => {
+    if(myQuizzesId.includes(quizz.id)){
+      user_Quizzes.innerHTML += `
+      <article>
+        <img src="${quizz.image}" alt="imagem do quizz" />
+        <div class="gradient"></div>
+        <p>${quizz.title}</p>
+      </article>`;
+    }
   });
 }
 
@@ -133,6 +184,7 @@ function createQuizz(quizz) {
     // a chave precisa pro bônus
     myQuizzes.push({ id: response.data.id, key: response.data.key });
     localStorage.setItem("quizzes", JSON.stringify(myQuizzes));
+    getAllQuizzes()
   });
 }
 
@@ -145,5 +197,7 @@ function deleteQuizz(ID) {
 }
 
 // createQuizz(quizz)
-getQuizz(2182);
-getAllQuizzes();
+// getQuizz(2182);
+  const promise = axios.post(QUIZZ_API, quizz);
+  promise.then(getAllQuizzes())
+// getAllQuizzes();
