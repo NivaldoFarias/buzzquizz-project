00ac3706f8047b2d63d0c94607570968f6110b06
Luiz Gustavo Santos Farias <lgsfarias.dev@gmail.com>
2022-02-20 22:08:08 -0300

    Delete quizz button working

--- scripts/script.js
+++ scripts/script.js
@@ -102,8 +102,8 @@ function printQuizz(quizzesArray, quizz, typeOfQuizz) {
     quizzesArray.innerHTML += `
     <article id="${quizz.id}">
       <div class="edit-delete-widget">
-        <ion-icon name="create-outline" id="delete-btn"></ion-icon>
-        <ion-icon name="trash-outline" id="edit-btn"></ion-icon>
+        <ion-icon name="create-outline" id="edit-btn"></ion-icon>
+        <ion-icon name="trash-outline" id="delete-btn"></ion-icon>
       </div>
       <img 
         src="${quizz.image}" 
@@ -152,6 +152,9 @@ function renderUserQuizzes() {
   user_QuizzesRenderedes.forEach((quizz) => {
     quizz.addEventListener("click", selectQuizz);
   });
+
+  let trashBtns = [...document.querySelectorAll("#delete-btn")];
+  trashBtns.forEach(trashBtn => trashBtn.addEventListener("click",deleteQuizz))
 }
 function getQuizz(ID) {
   const promise = axios.get(`${QUIZZ_API}/${ID}`);
@@ -176,12 +179,16 @@ function selectCreatedQuizz() {
     console.log(error.reponse.status);
   });
 }
-function deleteQuizz(ID) {
-  let key = myQuizzes.filter((quizz) => quizz.id == ID)[0].key;
-  const promise = axios.delete(`${QUIZZ_API}/${ID}`, {
+function deleteQuizz() {
+  let confirmation = confirm("Deseja mesmo apagar este Quizz?")
+  let key = myQuizzes.filter((quizz) => quizz.id == this.parentNode.parentNode.id)[0].key;
+  const promise = axios.delete(`${QUIZZ_API}/${this.parentNode.parentNode.id}`, {
     headers: { "Secret-Key": key },
   });
-  promise.then((response) => console.log(response));
+  promise.then((response) => {
+    console.log(response);
+    location.reload()
+  });
   promise.catch((error) => {
     console.log(error.reponse.status);
   });
@@ -959,89 +966,89 @@ function randomTimeOut() {
   return time;
 }
 
-/* quizz = {
-  title:
-    // "Só quem assistiu todos os filmes da Marvel vai gabaritar",
-    "juro que esse e o ultimo teste, nao aguento mais! misericordia!",
-  image:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png",
-  questions: [
-    {
-      title: "Que ator é conhecido pelo seu papel como O Hulk?",
-      color: "#15E818",
-      answers: [
-        {
-          text: "Mark Ruffalo",
-          image:
-            "https://ogimg.infoglobo.com.br/in/25320231-3fa-ec0/FT1086A/33973739_Marvels-AvengersAge-Of-UltronHulk-Bruce-Banner-Mark-RuffaloPhFilm-FrameMarvel.jpg",
-          isCorrectAnswer: true,
-        },
-        {
-          text: "Vincent D'Onofrio",
-          image:
-            "https://nerdhits.com.br/wp-content/uploads/2021/11/hulk-1.jpg",
-          isCorrectAnswer: false,
-        },
-      ],
-    },
-    {
-      title: "Qual é o nome do martelo encantado do Thor?",
-      color: "#1565E8",
-      answers: [
-        {
-          text: "Mjölnir",
-          image:
-            "https://exame.com/wp-content/uploads/2018/10/thor-ragnarok-filme-cultura-vip.jpg",
-          isCorrectAnswer: true,
-        },
-        {
-          text: "Mnajas",
-          image: "https://mega.ibxk.com.br/2013/11/04/04135704362.jpg",
-          isCorrectAnswer: false,
-        },
-      ],
-    },
-    {
-      title: "Em que ano foi lançado o primeiro filme do Homem de Ferro?",
-      color: "#E81515",
-      answers: [
-        {
-          text: "2008",
-          image:
-            "https://sm.ign.com/ign_br/news/m/marvels-ir/marvels-iron-man-vr-release-date-now-set-for-july-2020_8h12.jpg",
-          isCorrectAnswer: true,
-        },
-        {
-          text: "2010",
-          image:
-            "https://conteudo.imguol.com.br/c/entretenimento/96/2020/08/07/iron-man-1596813808466_v2_615x300.jpg",
-          isCorrectAnswer: false,
-        },
-      ],
-    },
-  ],
-  levels: [
-    {
-      title: "Sabe de nada!",
-      image:
-        "https://referencianerd.com/wp-content/uploads/2020/06/IronManSnapFunkoFeature.jpg",
-      text: "Você precisa maratonar os filmes da Marvel.",
-      minValue: 0,
-    },
-    {
-      title: "Foi por pouco!",
-      image:
-        "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/02/homem-de-ferro-tony-divulgacao.jpg",
-      text: "Você sabe bastante, mas precisa relembrar algo.",
-      minValue: 32,
-    },
-    {
-      title: "Sabe tudo!",
-      image:
-        "https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/11/Iron-Man-God-Armor-Infinity-Gauntlet.jpg",
-      text: "Você sabe de tudo da Marvel, já pode substituir o Vigia!",
-      minValue: 66,
-    },
-  ],
-}; */
+//  quizz = {
+//   title:
+//     // "Só quem assistiu todos os filmes da Marvel vai gabaritar",
+//     "juro que esse e o ultimo teste, nao aguento mais! misericordia!",
+//   image:
+//     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png",
+//   questions: [
+//     {
+//       title: "Que ator é conhecido pelo seu papel como O Hulk?",
+//       color: "#15E818",
+//       answers: [
+//         {
+//           text: "Mark Ruffalo",
+//           image:
+//             "https://ogimg.infoglobo.com.br/in/25320231-3fa-ec0/FT1086A/33973739_Marvels-AvengersAge-Of-UltronHulk-Bruce-Banner-Mark-RuffaloPhFilm-FrameMarvel.jpg",
+//           isCorrectAnswer: true,
+//         },
+//         {
+//           text: "Vincent D'Onofrio",
+//           image:
+//             "https://nerdhits.com.br/wp-content/uploads/2021/11/hulk-1.jpg",
+//           isCorrectAnswer: false,
+//         },
+//       ],
+//     },
+//     {
+//       title: "Qual é o nome do martelo encantado do Thor?",
+//       color: "#1565E8",
+//       answers: [
+//         {
+//           text: "Mjölnir",
+//           image:
+//             "https://exame.com/wp-content/uploads/2018/10/thor-ragnarok-filme-cultura-vip.jpg",
+//           isCorrectAnswer: true,
+//         },
+//         {
+//           text: "Mnajas",
+//           image: "https://mega.ibxk.com.br/2013/11/04/04135704362.jpg",
+//           isCorrectAnswer: false,
+//         },
+//       ],
+//     },
+//     {
+//       title: "Em que ano foi lançado o primeiro filme do Homem de Ferro?",
+//       color: "#E81515",
+//       answers: [
+//         {
+//           text: "2008",
+//           image:
+//             "https://sm.ign.com/ign_br/news/m/marvels-ir/marvels-iron-man-vr-release-date-now-set-for-july-2020_8h12.jpg",
+//           isCorrectAnswer: true,
+//         },
+//         {
+//           text: "2010",
+//           image:
+//             "https://conteudo.imguol.com.br/c/entretenimento/96/2020/08/07/iron-man-1596813808466_v2_615x300.jpg",
+//           isCorrectAnswer: false,
+//         },
+//       ],
+//     },
+//   ],
+//   levels: [
+//     {
+//       title: "Sabe de nada!",
+//       image:
+//         "https://referencianerd.com/wp-content/uploads/2020/06/IronManSnapFunkoFeature.jpg",
+//       text: "Você precisa maratonar os filmes da Marvel.",
+//       minValue: 0,
+//     },
+//     {
+//       title: "Foi por pouco!",
+//       image:
+//         "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/02/homem-de-ferro-tony-divulgacao.jpg",
+//       text: "Você sabe bastante, mas precisa relembrar algo.",
+//       minValue: 32,
+//     },
+//     {
+//       title: "Sabe tudo!",
+//       image:
+//         "https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/11/Iron-Man-God-Armor-Infinity-Gauntlet.jpg",
+//       text: "Você sabe de tudo da Marvel, já pode substituir o Vigia!",
+//       minValue: 66,
+//     },
+//   ],
+// }; 
 /* postQuizz(quizz) */
